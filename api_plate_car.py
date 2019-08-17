21b00af47cd7c7b12abcb905a21b3fdfbd8e1f3a
thalesgibbon <thales.gibbon@gmail.com>
2019-08-17 15:53:18 -0300

    api_plate_car

--- api_plate_car.py
+++ api_plate_car.py
@@ -1,21 +1,32 @@
+"""
+    como utilizar:
+        Plate2Str.process(im=r"img\anexo1.jpg")
+    retorno:
+        {'result': 'OK', 'plate': 'AQQ0J49', 'score': 0.896}
+"""
 import requests
 from __key__ import token
 
 
 class Plate2Str(object):
-    def __init__(self, im):
-        self.im = im
+    def __init__(self):
+        pass
 
-    def process(self):
+    def process(self, im):
         regions = ['fr', 'it']
-        with open(self.im, 'rb') as fp:
+        with open(im, 'rb') as fp:
             response = requests.post(
                 'https://api.platerecognizer.com/v1/plate-reader/',
                 data=dict(regions=regions),
                 files=dict(upload=fp),
                 headers={'Authorization': f'Token {token["car_plate"]}'})
             response = response.json()
-            plate = str(response['results'][0]['plate']).upper()
+            plate = str(response['results'][0]['plate']).upper().strip()
+            if len(plate) != 7:
+                return {'result': 'erro'}
             score = response['results'][0]['score']
-            dicio = {'result': 'ok', 'plate': plate, 'score': score}
+            dicio = {'result': 'OK', 'plate': plate, 'score': score}
         return dicio
+
+
+Plate2Str = Plate2Str()
