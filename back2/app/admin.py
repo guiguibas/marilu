1d98a05b697bda29cd42e3e263794ef772ccd8fd
Guilherme Neto <guigui.silva.neto@gmail.com.br>
2019-08-18 08:42:27 -0300

    Ultimo Commit
    Ultimo Commit

---
+++ back2/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import SimulaModel,CarroLojaModel
+
+admin.site.register(SimulaModel)
+admin.site.register(CarroLojaModel)
+
+# Register your models here.
